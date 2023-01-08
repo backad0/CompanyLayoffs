259458331c7dbd20bc05acaad03a36ce99efa4bc
backad0 <91736145+backad0@users.noreply.github.com>
2023-01-09 04:19:36 +0600

    remade for new algorithm

--- main.cpp
+++ main.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
+#include <algorithm>
 
 int main() {
-    int numOfEmpl, numOfLayoffs;
+    int numOfEmpl, numOfLayoffs, index;
     std::cin >> numOfEmpl >> numOfLayoffs;
-    if (numOfEmpl<numOfLayoffs) return 1;
-    int employers[numOfEmpl];
+    long long employers[numOfEmpl];
     for (int i = 0; i<numOfEmpl; i++){
         std::cin >> employers[i];
     }
-    int highestScores[numOfLayoffs];
+    /*long long highestScores[numOfLayoffs];
     for (int i = 0; i<numOfLayoffs;i++){
         std::cin >> highestScores[i];
+    }*/
+    long long sums[numOfEmpl];
+    sums[0] = employers[0];
+    for (int i = 1; i<=numOfEmpl; i++){
+        sums[i] = sums[i-1]+employers[i];
     }
-    int sum = 0;
-    for (int i = 0; i<numOfLayoffs; i++){
-        for (int j = 0; j < numOfEmpl; j++){
-            if (employers[j]>highestScores[i]){
-                employers[j] = highestScores[i];
+    for (int i = 0; i<numOfEmpl; i++){
+        //std::cout << "sum"<<i<<" = " << sums[i] << " ";
+    }
+    std::sort(employers, employers+numOfEmpl);
+    long long score;
+    //std::cout << std::endl;
+    while (numOfLayoffs--){
+        std::cin >> score;
+        for(int i=0;i<numOfEmpl;i++){
+            if(employers[i]>score){
+                index = i;
+                break;
             }
-            sum +=employers[j];
         }
-        std::cout << sum << std::endl;
-        sum = 0;
+        //std::cout << "index = " << index << std::endl;
+
+        if(index>=1) std::cout << sums[index-1]+(score*(numOfEmpl-index)) << std::endl;
+        else std::cout << score*numOfEmpl << std::endl;
     }
     return 0;
 }
